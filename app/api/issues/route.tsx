11071c92af3f3a7b01c4e30b7b2cf5156c6db8ad
walid-hamdi <walidhamdidev@gmail.com>
2023-10-30 18:56:02 +0100

    create issues api

---
+++ app/api/issues/route.tsx
@@ -0,0 +1,24 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
+
+const createIssusSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const issue = createIssusSchema.safeParse(body);
+  if (!issue.success)
+    return NextResponse.json(issue.error.errors, { status: 400 });
+
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json({ newIssue }, { status: 201 });
+}
