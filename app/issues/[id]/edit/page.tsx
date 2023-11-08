3492acf1a0cfeb3f49cf9491bf64391aaa7b0cd6
walid-hamdi <walidhamdidev@gmail.com>
2023-11-08 17:26:36 +0100

    Improve the loading experience

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
-import IssueForm from "../../_components/IssueForm";
 import prisma from "@/prisma/client";
+import dynamic from "next/dynamic";
 import { notFound } from "next/navigation";
+import IssueFormSkeleton from "./loading";
+const IssueForm = dynamic(() => import("../../_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 
 const EditIssuePage = async ({ params }: { params: { id: string } }) => {
   const issue = await prisma.issue.findUnique({
