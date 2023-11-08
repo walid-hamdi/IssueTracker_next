28fc4216f04b77bdc5d6982419bfb68bc900924a
walid-hamdi <walidhamdidev@gmail.com>
2023-11-08 20:09:59 +0100

    Handle Errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -4,57 +4,86 @@ import { AlertDialog, Button, Flex, Text } from "@radix-ui/themes";
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+
+  const deleteIssue = async () => {
+    try {
+      router.push("/issues");
+      await axios.delete(`/api/issues/${issueId}`);
+      router.refresh();
+    } catch (error) {
+      setError(true);
+    }
+  };
+
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Link
-          href={``}
-          className="bg-red-400 rounded-md text-white py-1 px-3 flex items-center justify-center"
-        >
-          <svg
-            width="15"
-            height="15"
-            viewBox="0 0 15 15"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Link
+            href={``}
+            className="bg-red-400 rounded-md text-white py-1 px-3 flex items-center justify-center"
           >
-            <path
-              d="M11.8536 1.14645C11.6583 0.951184 11.3417 0.951184 11.1465 1.14645L3.71455 8.57836C3.62459 8.66832 3.55263 8.77461 3.50251 8.89155L2.04044 12.303C1.9599 12.491 2.00189 12.709 2.14646 12.8536C2.29103 12.9981 2.50905 13.0401 2.69697 12.9596L6.10847 11.4975C6.2254 11.4474 6.3317 11.3754 6.42166 11.2855L13.8536 3.85355C14.0488 3.65829 14.0488 3.34171 13.8536 3.14645L11.8536 1.14645ZM4.42166 9.28547L11.5 2.20711L12.7929 3.5L5.71455 10.5784L4.21924 11.2192L3.78081 10.7808L4.42166 9.28547Z"
-              fill="currentColor"
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-          <Text className="pl-2">Delete</Text>
-        </Link>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Deletion confirmation</AlertDialog.Title>
-        <AlertDialog.Description>
-          Are you sure you want to delete?
-        </AlertDialog.Description>
-        <Flex gap="1" mt="4" justify="end">
-          <AlertDialog.Cancel>
-            <Button color="gray">Cancel</Button>
-          </AlertDialog.Cancel>
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11.8536 1.14645C11.6583 0.951184 11.3417 0.951184 11.1465 1.14645L3.71455 8.57836C3.62459 8.66832 3.55263 8.77461 3.50251 8.89155L2.04044 12.303C1.9599 12.491 2.00189 12.709 2.14646 12.8536C2.29103 12.9981 2.50905 13.0401 2.69697 12.9596L6.10847 11.4975C6.2254 11.4474 6.3317 11.3754 6.42166 11.2855L13.8536 3.85355C14.0488 3.65829 14.0488 3.34171 13.8536 3.14645L11.8536 1.14645ZM4.42166 9.28547L11.5 2.20711L12.7929 3.5L5.71455 10.5784L4.21924 11.2192L3.78081 10.7808L4.42166 9.28547Z"
+                fill="currentColor"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <Text className="pl-2">Delete</Text>
+          </Link>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Deletion confirmation</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete?
+          </AlertDialog.Description>
+          <Flex gap="1" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button color="gray">Cancel</Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red" onClick={deleteIssue}>
+                Delete Issue
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>
+            Something went wrong when you try to delete try again.
+          </AlertDialog.Description>
           <AlertDialog.Action>
             <Button
-              color="red"
-              onClick={async () => {
-                router.push("/issues");
-                await axios.delete(`/api/issues/${issueId}`);
-                router.refresh();
+              color="gray"
+              variant="soft"
+              mt="2"
+              onClick={() => {
+                setError(false);
               }}
             >
-              Delete Issue
+              Ok
             </Button>
           </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
