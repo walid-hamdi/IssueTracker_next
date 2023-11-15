3ad8b30d42f4f9901619a9e3a47c6c6dd9250285
walid-hamdi <walidhamdidev@gmail.com>
2023-11-15 18:55:37 +0100

    Fetch users with react query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,22 +1,27 @@
 "use client";
 
+import Skeleton from "@/app/components/Skeleton";
 import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { User } from "next-auth";
-import { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>();
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: async () =>
+      await axios.get<User[]>("/api/users").then((res) => res.data),
+    staleTime: 1000 * 60,
+    retry: 3,
+  });
 
-  useEffect(() => {
-    const fetch = async () => {
-      const { data: users } = await axios.get<User[]>("/api/users");
-      setUsers(users);
-      console.log(users);
-    };
+  if (error) return null;
 
-    fetch();
-  }, users);
+  if (isLoading) return <Skeleton />;
 
   return (
     <Select.Root>
@@ -25,7 +30,6 @@ const AssigneeSelect = () => {
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
           {users &&
-            users?.length > 0 &&
             users?.map((user) => {
               return <Select.Item value={user?.id}>{user?.name}</Select.Item>;
             })}
